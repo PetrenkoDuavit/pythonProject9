219c1efeb05590a63f3b1fd4e6a18bce35731e1d
Anton898 <81709784+IvanovAnto@users.noreply.github.com>
2024-10-29 12:20:34 +0200

    HW9.1

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+# Homewock 9.1
+def popular_words (text, words):
+    text = text.lower()
+    listWord = text.split()
+    result = {}
+    for i in words:
+        result[i] = listWord.count(i)
+         # print(f" \'{count}\':{listWord.count(count)}" )
+    return result
+
+
+print("Homeworck 9.1")
+
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
+print('OK')
+
+assert difference(1, 2, 3) == 2, 'Test1'
+assert difference(5, -5) == 10, 'Test2'
+assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+assert difference() == 0, 'Test4'
+print('OK')
